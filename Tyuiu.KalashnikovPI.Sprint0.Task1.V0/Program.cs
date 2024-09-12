dbc49670799e0e1e72c8109e0f688b717c60445b
kalashnikovpi <kalashnicovp@yandex.ru>
2024-09-12 20:24:54 +0500

    Task1

---
+++ Tyuiu.KalashnikovPI.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.KalashnikovPI.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Павел";
+            int age = 18;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
