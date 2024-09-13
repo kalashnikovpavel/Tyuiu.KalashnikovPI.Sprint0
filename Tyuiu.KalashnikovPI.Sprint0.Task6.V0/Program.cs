29781bb8a0dde427fb4deb6fa411527db52dacf2
kalashnikovpi <kalashnicovp@yandex.ru>
2024-09-13 22:28:46 +0500

    Task6

---
+++ Tyuiu.KalashnikovPI.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KalashnikovPI.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.KalashnikovPI.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элументов массива = " + DataService.AdditionArray(numsArray));
+
+            Console.WriteLine("Разность элементов массива = " + DataService.SubstractionArray(numsArray));
+
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+
+            Console.ReadKey();
+        }
+    }
+}
